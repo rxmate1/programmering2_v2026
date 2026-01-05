5eb1be421d949e62cbcb28da9cc0824160b11757
Ole-Edvard Ørebæk <ole.e.orebak@hiof.no>
2026-01-05 14:22:10 +0100

    Lecture 1 example code - Hello World

---
+++ Forelesning1/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+Dette er en helt grunnleggende kjørbar klasse i Java
+ */
+public class HelloWorld {
+    /*
+    Dette er en main()-metode. Dette er hvor vi definerer koden som skal kjøre i programmet vårt.
+    All kode som skal kjøres må kalles direkte eller inndirekte fra en slik main()-metode.
+    Hver klasse som inneholder en slik main()-metode kan anses som et eget og selvstendig program og vi kan ha mange
+    slike i et IntelliJ IDEA-prosjekt.
+    Ikke tenk på nøkkelord, slik som static, void, String[] osv. helt enda.
+     */
+    static void main(String[] args) {
+        // Dette skriver ut en linje til standard output.
+        System.out.println("Hello world!");
+    }
+}
---
+++ Forelesning1/HelloWorld/src/HelloWorld2.java
@@ -0,0 +1,9 @@
+/*
+Dette er en forkortet måte å skrive en kjørbar klasse.
+Vi ser her at det ikke er en klassedefinisjon, bare en main()-metode.
+Dette funker i Java 25 (JDK 25), men vil ikke nødvendigvis fungere i tidligere Java-versjoner.
+Jeg anbefaler derfor å likevel benytte gamlemåten (Se HelloWorld.java), som er støttet i alle relevante Java-versjoner.
+ */
+void main() {
+    System.out.println("Hello world 2!");
+}
